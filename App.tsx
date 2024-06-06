71886ef462452f12fb47e347ad5488342887276c
l3miage-xusi <121896780+l3miage-xusi@users.noreply.github.com>
2024-06-06 22:41:39 +0200

    navigation list favori

--- App.tsx
+++ App.tsx
@@ -1,27 +1,32 @@
-import React, {PropsWithChildren} from 'react';
+import React from 'react';
 import {
     Image,
     SafeAreaView,
     StatusBar,
     StyleSheet, TouchableOpacity,
     useColorScheme,
+    DrawerLayoutAndroid,
     View,
+    Text, GestureResponderEvent,
 } from 'react-native';
 import {NavigationContainer, useNavigation} from "@react-navigation/native";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Home from "./component/Home.tsx";
 import Profile from "./component/Profile.tsx";
 import Icon from "react-native-vector-icons/MaterialIcons";
 import {StackNavigationProp} from "@react-navigation/stack";
+import ListFavori from "./component/ListFavori.tsx";
 
 type RootStackParamList = {
     Home: undefined;
     Profile: undefined;
+    Favori: undefined;
 };
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App = (): React.JSX.Element => {
+    const drawer = React.useRef<DrawerLayoutAndroid>(null);
     const isDarkMode = useColorScheme();
     const backgroundStyle = {
         backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -34,22 +39,46 @@ const App = (): React.JSX.Element => {
                 backgroundColor={backgroundStyle.backgroundColor}
             />
             <NavigationContainer>
-                <HeaderBar />
-                    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-                        <Stack.Screen name="Home" component={Home} />
-                        <Stack.Screen name="Profile" component={Profile} />
+                <DrawerLayoutAndroid
+                    ref={drawer}
+                    drawerWidth={300}
+                    drawerPosition={'left'}
+                    renderNavigationView={() => <NavigationView drawerRef={drawer} />}
+                >
+                    <HeaderBar onPress={() => drawer.current?.openDrawer()}/>
+                    <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+                        <Stack.Screen name="Home" component={Home}/>
+                        <Stack.Screen name="Profile" component={Profile}/>
+                        <Stack.Screen name="Favori" component={ListFavori}/>
                     </Stack.Navigator>
+                </DrawerLayoutAndroid>
             </NavigationContainer>
         </SafeAreaView>
     );
 }
 
-const HeaderBar = () => {
+// @ts-ignore
+const NavigationView = ({drawerRef}) => {
+    const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+    return (
+        <View style={[styles.container, styles.navigationContainer]}>
+            <TouchableOpacity onPress={() => {
+                navigation.navigate('Favori');
+                if (drawerRef) {
+                    drawerRef.current?.closeDrawer();
+                }
+            }}>
+                <Text style={styles.paragraph}>Ma liste favori</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+const HeaderBar = (props: { onPress: ((event: GestureResponderEvent) => void) | undefined; }) => {
     const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
     return(
         <View style={styles.headerContainer}>
-            <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
-                <Icon name="person" size={50} color="#900" />
+            <TouchableOpacity onPress={props.onPress}>
+                <Icon name="menu" size={30} color="#900" />
             </TouchableOpacity>
             <TouchableOpacity onPress={() => navigation.navigate('Home')}>
             <Image
@@ -57,8 +86,8 @@ const HeaderBar = () => {
                 style={styles.logo}
             />
             </TouchableOpacity>
-            <TouchableOpacity>
-                <Icon name="list" size={50} color="#900" />
+            <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+                <Icon name="person" size={50} color="#900" />
             </TouchableOpacity>
         </View>
     );
@@ -82,6 +111,25 @@ const styles = StyleSheet.create({
     logo: {
         width: 250,
         resizeMode: 'contain'
+    },
+    picker: {
+        flex: 0.6,
+        height: 50,
+    },
+    dropdown: {
+        width: 200,
+        height: 50,
+        backgroundColor: 'white',
+        borderRadius: 8,
+        paddingHorizontal: 10
+    },
+    navigationContainer: {
+        backgroundColor: '#ecf0f1',
+    },
+    paragraph: {
+        padding: 16,
+        fontSize: 15,
+        textAlign: 'center',
     },
 });
 
--- component/ListFavori.tsx
+++ component/ListFavori.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {Image, StyleSheet, Text, View} from "react-native";
+import React from "react";
 
-function Profile() {
+const ListFavori = () => {
+
     return (
         <View style={styles.container}>
-            <Image
-                source={require('../asset/Emi.png')}
-                style={styles.image}
-            />
             <View style={styles.textContainer}>
-                <Text>Emi</Text>
+                <Text>LIST FAVORI</Text>
             </View>
-
         </View>
-    );
+    )
 }
 
 const styles = StyleSheet.create({
@@ -23,18 +19,11 @@ const styles = StyleSheet.create({
         paddingTop: 10,
         margin: 15
     },
-    image: {
-        width: 200,
-        height: 200,
-        marginRight: 10,
-        marginTop: 10
-    },
     textContainer: {
         flex: 1,
         justifyContent: 'flex-start',
         paddingTop: 20,
         paddingLeft: 20
     }
-});
-
-export default Profile;
+})
+export default ListFavori
